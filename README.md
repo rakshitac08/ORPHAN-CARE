16c7815c6524480925322ec593f003a9377969a3
rakshitac08 <rakshitachand08@gmail.com>
2025-08-31 16:44:23 +0530

    Initial commit by me

---
+++ README.md
@@ -0,0 +1,16 @@
+# ORPHAN-CARE
+
+Orphan-Care is a robust, web-based application designed to efficiently manage records of orphans. Leveraging PHP for backend development and MySQL for database management ensures secure data handling and reliable performance. Additionally, it features an integrated payment gateway for secure donation processing.
+
+
+- Orphan Records Management: Comprehensive storage and management of orphan details.
+- Donation Management: Integrated payment gateway for accepting donations.
+- Cross-Platform Compatibility: Compatible with XAMPP for Windows.
+
+Technologies Used:
+
+- Frontend: HTML, CSS, JavaScript
+- Backend: PHP
+- Database: MySQL
+- Server: XAMPP
+
---
+++ admin/admin_components/admin_side-menu.php
@@ -0,0 +1,12 @@
+<div class="four wide column" id="example1">
+    <div class="ui secondary vertical pointing menu">
+        <a class="item <?php echo ($_SERVER['PHP_SELF'] == "/orphan/admin/index.php" ? "active" : "");?>" href="index.php">Dashboard</a>
+        <a class="item <?php echo ($_SERVER['PHP_SELF'] == "/orphan/admin/children.php" ? "active" : "");?>" href="./children.php">Children</a>
+        <a class="item <?php echo ($_SERVER['PHP_SELF'] == "/orphan/admin/sponsorer.php" ? "active" : "");?>" href="./sponsorer.php">Sponsorers</a>
+        <a class="item <?php echo ($_SERVER['PHP_SELF'] == "/orphan/admin/donators.php" ? "active" : "");?>" href="donators.php">Donators</a>
+        <a class="item <?php echo ($_SERVER['PHP_SELF'] == "/orphan/admin/gift-sent.php" ? "active" : "");?>" href="gift-sent.php">Gift Sent</a>
+        <a class="item <?php echo ($_SERVER['PHP_SELF'] == "/orphan/admin/programs.php" ? "active" : "");?>" href="./programs.php">Programs</a>
+        <a class="item <?php echo ($_SERVER['PHP_SELF'] == "/orphan/admin/feedback.php" ? "active" : "");?>" href="feedback.php">Feedback</a>
+        <a class="item <?php echo ($_SERVER['PHP_SELF'] == "/orphan/admin/child-requests.php" ? "active" : "");?>" href="child-requests.php">Child-requests</a>
+    </div>
+</div>
